2791f26ff471aaaa226cb25b6e2053716fb3fef4
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2024-06-08 16:07:04 +0300

    Slack got terrible

--- action_reports.py
+++ action_reports.py
@@ -2,10 +2,9 @@
 import re
 from argparse import ArgumentParser, Namespace
 from collections import defaultdict, namedtuple
-from functools import reduce
 from datetime import date, timedelta
-from statistics import mean
 from types import SimpleNamespace
+import matplotlib.pyplot as plt
 
 from helpers.generic import git_log_actions, load_config, reverse_workdays
 from helpers.slack import SlackClient
@@ -30,96 +29,91 @@ def publish_blocks(config: SimpleNamespace, data: dict):
 
 def main(args: Namespace, config: SimpleNamespace):
     ## Use settings and arguments
-    last = config.today
-    first = last - timedelta(days=7 * 8)
-    ignore_pattern = re.compile(config.ignore_file_pattern)
-    ticket_pattern = re.compile(config.ticket_pattern)
-    skip = set()
-
-    ## Merge and index all GIT commits by date
-    commits_index = defaultdict(list)
-    seen_files = set()
-    seen_authors = set()
-    for repo in config.repositories:
-        history, commit_actions = git_log_actions(
-            repo, start=first, end=last, skip_files=ignore_pattern
-        )
-        for commit in history:
-            seen_authors.add(commit.author.title())
-
-            insertions, deletions = 0, 0
-            for action in commit_actions[commit.hash]:
-                seen_files.add(action.path)
-                insertions += int(action.insertions)
-                deletions += int(action.deletions)
-            a_date = date.fromisoformat(commit.date)
-
-            tickets = set(ticket_pattern.findall(commit.message))
-
-            commits_index[commit.date].append(
-                AuthorCommitStats(
-                    a_date, commit.author.title(), insertions, deletions, tickets
-                )
-            )
-
-    ## Build weekly stats
-    author_weekly = {author: defaultdict(default_stats) for author in seen_authors}
-    weeks = set()
-    fridays = set()
-    for day in reverse_workdays(first, last, skip=skip):
-        day_str = day.isoformat()
-        week = day.isocalendar().week
-        weeks.add(week)
-
-        days_to_friday = 4 - day.weekday()
-        friday = day + timedelta(days=days_to_friday)
-        fridays.add(friday.isoformat())
-
-        for ci in commits_index[day_str]:
-            prev = author_weekly[ci.author][week]
-            author_weekly[ci.author][week] = Stats(
-                prev.insertions + ci.insertions,
-                prev.deletions + ci.deletions,
-                prev.tickets | ci.tickets,
-            )
-
-    ## Prepare slack blocks
-    head_block = dict(
-        type="section",
-        text=dict(
-            type="mrkdwn",
-            text=f"Weekly git commit stats up-to *{config.today}*.\nLegend: (+) insertions, (-) deletetions, (t) tickets",
-        ),
-    )
-    pretty_weeks = ["Week", "Average", *sorted(fridays)]
-    author_blocks = dict(
-        type="section",
-        fields=[
-            {"type": "mrkdwn", "text": "\n".join(pretty_weeks)},
-        ],
-    )
-
-    ## Rating authors
-    for author, weekly in author_weekly.items():
-        insertions = round(mean(s.insertions for s in weekly.values()), 1)
-        deletions = round(mean(s.deletions for s in weekly.values()), 1)
-        tickets = reduce(lambda acc, el: acc | el, (s.tickets for s in weekly.values()))
-        print(author, "Average", Stats(insertions, deletions, len(tickets)))
-        lines = [
-            author,
-            f"(+) {insertions}, (-) {deletions}, (t) {len(tickets)}",
-        ]
-
-        for week in sorted(weeks):
-            print(week, weekly[week])
-            lines.append(
-                f"(+) {weekly[week].insertions}, (-) {weekly[week].deletions}, (t) {len(weekly[week].tickets)}"
-            )
-
-        author_blocks["fields"].append(dict(type="mrkdwn", text="\n".join(lines)))
+    # last = config.today
+    # first = last - timedelta(days=7 * 8)
+    # ignore_pattern = re.compile(config.ignore_file_pattern)
+    # ticket_pattern = re.compile(config.ticket_pattern)
+    # skip = set()
+    #
+    # ## Merge and index all GIT commits by date
+    # commits_index = defaultdict(list)
+    # seen_files = set()
+    # seen_authors = set()
+    # for repo in config.repositories:
+    #     history, commit_actions = git_log_actions(
+    #         repo, start=first, end=last, skip_files=ignore_pattern
+    #     )
+    #     for commit in history:
+    #         seen_authors.add(commit.author.title())
+    #
+    #         insertions, deletions = 0, 0
+    #         for action in commit_actions[commit.hash]:
+    #             seen_files.add(action.path)
+    #             insertions += int(action.insertions)
+    #             deletions += int(action.deletions)
+    #         a_date = date.fromisoformat(commit.date)
+    #
+    #         tickets = set(ticket_pattern.findall(commit.message))
+    #
+    #         commits_index[commit.date].append(
+    #             AuthorCommitStats(
+    #                 a_date, commit.author.title(), insertions, deletions, tickets
+    #             )
+    #         )
+    #
+    # ## Build weekly stats
+    # author_weekly = {
+    #     author: defaultdict(default_stats) for author in sorted(seen_authors)
+    # }
+    # weeks = set()
+    # fridays = set()
+    # for day in reverse_workdays(first, last, skip=skip):
+    #     day_str = day.isoformat()
+    #     week = day.isocalendar().week
+    #     weeks.add(week)
+    #
+    #     days_to_friday = 4 - day.weekday()
+    #     friday = day + timedelta(days=days_to_friday)
+    #     fridays.add(friday.isoformat())
+    #
+    #     for ci in commits_index[day_str]:
+    #         prev = author_weekly[ci.author][week]
+    #         author_weekly[ci.author][week] = Stats(
+    #             prev.insertions + ci.insertions,
+    #             prev.deletions + ci.deletions,
+    #             prev.tickets | ci.tickets,
+    #         )
+    #
+    # # Extract all weeks
+    # weeks = sorted({week for weekly in author_weekly.values() for week in weekly})
+    #
+    # # Prepare data for plotting
+    # plt.figure(figsize=(14, 8))
+    #
+    # # Calculate the metrics and plot for each author
+    # markers = ["x", "*", "o", "+", "d"]
+    # for i, (author, weekly) in enumerate(author_weekly.items()):
+    #     metrics = [
+    #         (stats.insertions * 3 + stats.deletions) / 4
+    #         for week in weeks
+    #         for stats in [weekly[week]]
+    #     ]
+    #     plt.plot(weeks, metrics, marker=markers[i % len(markers)], label=author)
+    #
+    # plt.xlabel("Weeks")
+    # plt.ylabel("Metric")
+    # plt.title("Weekly Metrics per Author")
+    # plt.xticks(weeks, [f"Week {week}" for week in weeks], rotation=45)
+    # plt.legend()
+    # plt.tight_layout()
+    #
+    # plt.savefig(f"weekly_metrics_{config.today}.png")
 
     slack = SlackClient(config.slack.bot_token, config.slack.channel)
-    slack.send_blocks([head_block, author_blocks])
+    slack.upload_image(
+        f"Weekly metrics up-to {config.today}",
+        f"weekly_metrics_{config.today}.png"
+    )
 
 
 if __name__ == "__main__":
--- helpers/slack.py
+++ helpers/slack.py
@@ -5,14 +5,16 @@ import requests
 class SlackClient:
     def __init__(self, token: str, channel: str) -> None:
         self.headers = {
-            "Content-Type": "application/json",
+            "Content-Type": "application/json; charset=utf-8",
             "Authorization": f"Bearer {token}",
         }
         self.url = "https://slack.com/api/chat.postMessage"
         self.channel = channel
 
     def _send(self, payload: dict):
-        response = requests.post(self.url, headers=self.headers, data=json.dumps(payload))
+        response = requests.post(
+            self.url, headers=self.headers, data=json.dumps(payload)
+        )
 
         if response.status_code == 200:
             response_data = response.json()
@@ -27,10 +29,49 @@ class SlackClient:
         payload = {"channel": self.channel, "text": text}
         self._send(payload)
 
-
     def send_blocks(self, blocks: list[str]):
         payload = {"channel": self.channel, "blocks": blocks}
         self._send(payload)
+
+    def upload_image(self, title: str, file_path: str):
+        with open(file_path, "rb") as file:
+            file_content = file.read()
+
+        payload = dict(filename=file_path, length=len(file_content))
+        response = requests.get(
+            "https://slack.com/api/files.getUploadURLExternal",
+            headers=self.headers,
+            params=payload,
+        )
+        if response.status_code != 200:
+            raise RuntimeError("Cannot get upload url, reason", response.text)
+        upload_reply = response.json()
+        if not upload_reply.get("ok"):
+            raise RuntimeError("Failed to get upload url, reason", upload_reply)
+
+        response = requests.post(
+            upload_reply.get("upload_url"),
+            headers=self.headers,
+            files=dict(file=(file_path, file_content, "image/png")),
+        )
+        if response.status_code != 200:
+            raise RuntimeError(
+                "Image content upload has failed, reason:", response.text
+            )
+        else:
+            print("Image content uploaded successfully.")
+
+        payload = dict(
+            files=[dict(id=upload_reply["file_id"], title=title)],
+            channel_id=self.channel,
+            initial_comment="initial",
+        )
+        response = requests.post(
+            "https://slack.com/api/files.completeUploadExternal",
+            headers=self.headers,
+            data=json.dumps(payload),
+        )
+        print(response.json())
 
 
 if __name__ == "__main__":
